11c11320441325c17a2d59f390c98a0cbdf50a34
Puk06 <86549420+puk06@users.noreply.github.com>
2025-09-23 20:16:51 +0900

    ちょっと変更

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Pukorufu - Blog",
-	subtitle: "Pukorufu's Blog",
+	title: "ぷこるふのぶろぐ",
+	subtitle: "",
 	lang: "ja", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
