037d2e2dfcec19b25e494a2828c152fdb795c586
Puk06 <86549420+puk06@users.noreply.github.com>
2025-09-24 07:59:52 +0900

    サイトURLの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.pukosrv.net/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
